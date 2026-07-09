a62a16ae85240944f746c3c14c8f608d6b723ac4
sonalverma192 <sonalverma91100@gmail.com>
2026-07-09 22:01:45 +0530

    last

--- Router/contact.js
+++ Router/contact.js
@@ -6,9 +6,9 @@ const jwt = require('jsonwebtoken')
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-   cloud_name : process.env.CLOUD_NAME,
-   api_key : process.env.API_KEY,
-   api_secret : process.env.API_SECRET
+   cloud_name: process.env.CLOUD_NAME,
+   api_key: process.env.API_KEY,
+   api_secret: process.env.API_SECRET
 })
 
 //  add contact
@@ -34,112 +34,113 @@ cloudinary.config({
 //             error : 'something is wrong'
 //          })
 //       })
-      
+
 //  })
 
 //  add-contact 
 
-Router.post('/add-contact', async(req,res)=>{
-   try
-   {
+Router.post('/add-contact', async (req, res) => {
+   try {
       // console.log(req.headers.authorization.split(" ")[1])
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
       console.log(tokenData)
+      console.log("Authorization:", req.headers.authorization);
+      console.log("Body:", req.body);
+      console.log("Files:", req.files);
+      console.log("Photo:", req.files?.photo);
       const uploadResult = await cloudinary.uploader.upload(req.files.photo.tempFilePath)
+      console.log("Uploading to Cloudinary...");
       console.log(uploadResult)
-     const newContact = new Contact({
-        fullName : req.body.fullName,
-        email : req.body.email,
-        phone : req.body.phone,
-        address : req.body.address,
-        gender: req.body.gender,
-        userId: tokenData.userId,
-        imageId : uploadResult.public_id,
-        imageUrl : uploadResult.secure_url
-       })
-    
-    const newData = await newContact.save()
-    res.status(200).json({
-      result : newData
-    })
+      const newContact = new Contact({
+         fullName: req.body.fullName,
+         email: req.body.email,
+         phone: req.body.phone,
+         address: req.body.address,
+         gender: req.body.gender,
+         userId: tokenData.userId,
+         imageId: uploadResult.public_id,
+         imageUrl: uploadResult.secure_url
+      })
+
+      const newData = await newContact.save()
+      res.status(200).json({
+         result: newData
+      })
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-        error : err
+         error: err
       })
    }
 })
 
 
 // get all contact
-Router.get('/all-contact',async(req,res)=>{
-   try{
+Router.get('/all-contact', async (req, res) => {
+   try {
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
-      const allContact = await Contact.find({userId:tokenData.userId}).select("_id fullName email phone address gender userId").populate('userId','fullName email')
-       res.status(200).json({
-         contacts : allContact
-       })
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
+      const allContact = await Contact.find({ userId: tokenData.userId }).select("_id fullName email phone address gender userId").populate('userId', 'fullName email')
+      res.status(200).json({
+         contacts: allContact
+      })
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err
+         error: err
       })
    }
 })
 
 
 // get contact by id 
-Router.get('/Contact/:id',async(req,res)=>{
-   try{
+Router.get('/Contact/:id', async (req, res) => {
+   try {
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
       const id = req.params.id
       // const data = await Contact.findById(id).select("fullName address")
-      const data = await Contact.find({_id:req.params.id,userId:tokenData.userId})
-          return res.status(200).json({
-         contact : data.length>0 ? data[0] : {} 
+      const data = await Contact.find({ _id: req.params.id, userId: tokenData.userId })
+      return res.status(200).json({
+         contact: data.length > 0 ? data[0] : {}
       })
 
    }
-   catch(err){
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err 
+         error: err
       })
    }
 })
 
 
 // get contact by gender
-Router.get('/gender/:g',async(req,res)=>{
-   try{
+Router.get('/gender/:g', async (req, res) => {
+   try {
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
-      const contact = await Contact.find({gender:req.params.g,userId:tokenData.userId})
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
+      const contact = await Contact.find({ gender: req.params.g, userId: tokenData.userId })
       res.status(200).json({
-         contact : contact
+         contact: contact
       })
 
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err
+         error: err
       })
    }
 })
 
 
 // delete contact by id 
-Router.delete('/:id',async(req,res)=>{
-   try{
+Router.delete('/:id', async (req, res) => {
+   try {
       // const token = req.headers.authorization.split(" ")[1]
       // const tokenData = await jwt.verify(token,process.env.SEC_KEY)
       // await Contact.deleteOne({_id:req.params.id,userId:tokenData.userId})
@@ -148,127 +149,116 @@ Router.delete('/:id',async(req,res)=>{
       // ])
 
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
       const contact = await Contact.findById(req.params.id)
-      if(contact.userId != tokenData.userId)
-      {
+      if (contact.userId != tokenData.userId) {
          return res.status(500).json({
-            msg:"invalid user"
+            msg: "invalid user"
          })
       }
 
       await cloudinary.uploader.destroy(contact.imageId)
-      await Contact.deleteOne({_id:req.params.id,userId:tokenData.userId})
-       res.status(200).json({
-         msg:"data delete"
-   })
+      await Contact.deleteOne({ _id: req.params.id, userId: tokenData.userId })
+      res.status(200).json({
+         msg: "data delete"
+      })
 
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err
+         error: err
       })
    }
 })
 
 
 // delete many by particular category/ies
-Router.delete('/byGender/:g',async(req,res)=>{
-   try
-   {
+Router.delete('/byGender/:g', async (req, res) => {
+   try {
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
-      await Contact.deleteMany({gender:req.params.g,userId:tokenData.userId})
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
+      await Contact.deleteMany({ gender: req.params.g, userId: tokenData.userId })
       res.status(200).json({
-         msg : `All contactof this gender is deleted .....`
+         msg: `All contactof this gender is deleted .....`
       })
 
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err
+         error: err
       })
    }
-}) 
+})
 
 // update contact
-Router.put('/update/:id',async(req,res)=>{
-   try
-   {
+Router.put('/update/:id', async (req, res) => {
+   try {
       console.log(req.body)
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
       const contactData = await Contact.findById(req.params.id)
-      if(contactData.userId != tokenData.userId)
-      {
+      if (contactData.userId != tokenData.userId) {
          return res.status(500).json({
-            msg:'you dont have permission to update this data'
+            msg: 'you dont have permission to update this data'
          })
       }
       // console.log(contactData)
       const newData = ({
-         fullName:req.body.fullName,
-         email:req.body.email,
-         gender:req.body.gender,
-         address:req.body.address,
-         userId:req.body.userId,
-         phone:req.body.phone
+         fullName: req.body.fullName,
+         email: req.body.email,
+         gender: req.body.gender,
+         address: req.body.address,
+         userId: req.body.userId,
+         phone: req.body.phone
       })
 
-      if(req.files)
-      {
+      if (req.files) {
          cloudinary.uploader.destroy(contactData.imageId)
          const uploadResult = await cloudinary.uploader.upload(req.files.photo.tempFilePath)
-         newData["imageId"]= uploadResult.public_id,
-         newData["imageUrl"] = uploadResult.secure_url
+         newData["imageId"] = uploadResult.public_id,
+            newData["imageUrl"] = uploadResult.secure_url
       }
-      else
-      {
+      else {
          newData["imageId"] = contactData.imageId,
-         newData["imageUrl"] = contactData.imageUrl
+            newData["imageUrl"] = contactData.imageUrl
       }
 
-      const updateContact = await Contact.findByIdAndUpdate(req.params.id,newData,{new:true})
+      const updateContact = await Contact.findByIdAndUpdate(req.params.id, newData, { new: true })
       res.status(200).json({
-         msg:'contact updated',
-         result:updateContact
+         msg: 'contact updated',
+         result: updateContact
       })
 
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error:err
+         error: err
       })
    }
 })
 
 // count contact
-Router.get('/count',async(req,res)=>{
-   try
-   {
+Router.get('/count', async (req, res) => {
+   try {
       const token = req.headers.authorization.split(" ")[1]
-      const tokenData = await jwt.verify(token,process.env.SEC_KEY)
-      const data = await Contact.countDocuments({userId:tokenData.userId})
+      const tokenData = await jwt.verify(token, process.env.SEC_KEY)
+      const data = await Contact.countDocuments({ userId: tokenData.userId })
       res.status(200).json({
-         count : data
+         count: data
       })
    }
-   catch(err)
-   {
+   catch (err) {
       console.log(err)
       res.status(500).json({
-         error : err
+         error: err
       })
    }
-   
+
 })
 
 
-     
-   module.exports = Router
+
+module.exports = Router
